c97bab0b1ddfa856e649bc1af548b0f895171422
Danil <super.danil501@gmail.com>
2024-08-23 17:26:05 -0600

    Add project files.

---
+++ sp-hw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("gdi32.dll")]
+    static extern int GetDeviceCaps(IntPtr hdc, int Index);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetDC(IntPtr hWnd);
+
+    private static double GetScreenDiagonalUsingAPI()
+    {
+        //Дескриптор всего экрана
+        IntPtr hdc = GetDC(IntPtr.Zero);
+
+        double widthInInches = GetDeviceCaps(hdc, 4) / 25.4; //4 - HORZSIZE
+        double heightInInches = GetDeviceCaps(hdc, 6) / 25.4; //6 - VERTSIZE
+
+        double diagonalInInches = Math.Sqrt(Math.Pow(widthInInches, 2) + Math.Pow(heightInInches, 2));
+        return diagonalInInches;
+    }
+
+    static void Main(string[] args)
+    {
+        double diagonal = GetScreenDiagonalUsingAPI();
+        Console.WriteLine($"Screen diagonal: {diagonal:F1} inches");
+        Console.ReadKey();
+    }
+}
+
---
+++ sp-hw1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sp-hw1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sp-hw1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ef3fa42-8c63-49ca-ad4e-350040d577c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
